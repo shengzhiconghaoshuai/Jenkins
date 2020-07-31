c0c93f60d545a59dc746586284422696af912014
shengzhicong <shengzhicong@wuxicloud.com>
2020-07-31 16:40:20 +0800

    第一次提交

---
+++ src/main/java/com/example/jenkinsdemo/Demo.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Demo {
+    @RequestMapping(method = RequestMethod.GET)
+    public String test() {
+        return "hello";
+    }
+}
---
+++ src/test/java/com/example/jenkinsdemo/JenkinsdemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.jenkinsdemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class JenkinsdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
